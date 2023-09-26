d03c6e35ba0a92be23f706efcebb5a5ceb11bf1c
Jackeramol <142860994+Jackeramol@users.noreply.github.com>
2023-09-27 00:25:56 +0530

    Add files via upload

---
+++ Assignment01_Ques4/Assignment01_Ques4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Write a c# program that count the even and odd elememnt of an integer array using loop and if else statement
+namespace Assignment01_Ques4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count_even = 0;
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    count_even++;
+                }
+
+            }
+            int count_odd = n - count_even;
+            Console.WriteLine("number of even element in the array is :- " + count_even);
+            Console.WriteLine("Number of odd element in the array is :- " + count_odd);
+
+        }
+    }
+}
