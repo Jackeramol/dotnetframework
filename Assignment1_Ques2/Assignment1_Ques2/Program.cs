eb93066c25f72fd39b62373772e0db222426e621
Jackeramol <142860994+Jackeramol@users.noreply.github.com>
2023-09-26 23:23:02 +0530

    Add files via upload

---
+++ Assignment1_Ques2/Assignment1_Ques2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Create a c# program that calculates the average value in a floating point array in a loop
+namespace Assignment1_Ques2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            float sum = 0;
+            float[] arr = new float[n];
+            for(int i = 0; i < n; i++)
+            {
+                arr[i] = (float)Convert.ToDouble(Console.ReadLine());
+                
+            }
+            for (int i = 0; i < n; i++)
+            {
+                sum += arr[i];
+
+            }
+            float average = sum / n;
+            Console.WriteLine(average);
+        }
+    }
+}
