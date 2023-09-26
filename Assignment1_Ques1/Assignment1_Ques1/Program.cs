a9c2664b108b039ef51614dfca688b8d16300040
Jackeramol <142860994+Jackeramol@users.noreply.github.com>
2023-09-26 23:04:19 +0530

    Add files via upload

---
+++ Assignment1_Ques1/Assignment1_Ques1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Write a c sharp program to find the sum of all element in an integer array using a loop */
+namespace Assignment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                sum += arr[i];
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
